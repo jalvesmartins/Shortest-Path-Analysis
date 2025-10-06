009e881632c390f05a11f3b4cf408f026a800871
jalvesmartins16 <jalvesmartins16@gmail.com>
2025-10-06 14:48:32 -0300

    feat: adiciona função de leitura do grafo.

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -12,5 +12,24 @@ class Graph:
 
     # Adiciona uma aresta ao grafo.
     def add_edge(self, u, v, weight, edge_id):
-        edge = Edge(id=edge_id, to=v, weight=weight)
-        self.adjacency_list[u].append(edge)
+        # Cria arestas para as duas direções.
+        edge1 = Edge(id=edge_id, to=v, weight=weight)
+        edge2 = Edge(id=edge_id, to=u, weight=weight)
+        
+        # Adiciona aresta nas duas listas.
+        self.adjacency_list[u].append(edge1)
+        self.adjacency_list[v].append(edge2)
+
+    # Faz a leitura completa do grafo.
+    def read_graph(self):
+        # Lê o tamanho do grafo (N vértices e M arestas).
+        N, M = map(int, input().split())
+
+        # Adiciona os N vértices.
+        for i in range(N):
+            self.add_vertex(i)
+
+        # Adiciona as M arestas, com o i sendo o id de cada aresta.
+        for i in range(1, M+1):
+            u, v, weight = map(int, input().split())
+            self.add_edge(u, v, weight, i)
