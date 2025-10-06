d007fe6bc78995f0d15f5d5e94c611531cec1ef1
jalvesmartins16 <jalvesmartins16@gmail.com>
2025-10-06 11:50:29 -0300

    feat: implementação classe edge.

---
+++ src/graph/edge.py
@@ -0,0 +1,11 @@
+class Edge:
+    # Construtor da classe, recebe id, nó de destino e peso.
+    def __init__(self, id, to, weight):
+        self.id = id
+        self.to = to
+        self.weight = weight
+
+    # Função para facilitar o print da aresta.
+    def __repr__(self):
+        return f" self.id"
+    
